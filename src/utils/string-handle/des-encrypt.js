5c9cff766946d9c132057bdef50a4baaf890a5f8
duanxiaojie <18435179812@163.com>
2023-01-03 11:27:50 +0800

    base_build

--- src/utils/string-handle/des-encrypt.js
+++ src/utils/string-handle/des-encrypt.js
@@ -2,7 +2,7 @@ import CryptoJS from 'crypto-js';
 
 export default function (str) {
   if (!CryptoJS) return '';
-  const keyHex = CryptoJS.enc.Utf8.parse('__jDlog__');
+  const keyHex = CryptoJS.enc.Utf8.parse('__jDlog__085169431');
   const encrypted = CryptoJS.DES.encrypt(str, keyHex, {
     mode: CryptoJS.mode.ECB,
     padding: CryptoJS.pad.Pkcs7,
